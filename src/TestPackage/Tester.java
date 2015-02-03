06cca67b0f76271bc9afcc7eeeeaa28000ad4e64
aarahman <aarahman@ncsu.edu>
2015-02-03 10:36:09 -0500

    hello world commit

---
+++ src/TestPackage/Tester.java
@@ -0,0 +1,11 @@
+package TestPackage;
+
+public class Tester {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World ! ");
+	}
+
+}
